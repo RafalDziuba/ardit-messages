8d0d53f7e855b2be3a8ed271855fb10051fe53a5
RafalDziuba <72468641+RafalDziuba@users.noreply.github.com>
2022-10-05 10:13:38 +0200

    Adding messages, read contract state (without UI)

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>{{ store.contractId }}</h1>
+    <form>
+      <input type="text" v-model="content" />
+      <button @click.prevent="sendContent">Send</button>
+    </form>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { useContractStore } from './stores/contract';
+import { ref } from 'vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+let content = ref('');
+const store = useContractStore();
+store.getContract();
+const sendContent = () => {
+  if (content.value == '') {
+    return;
+  } else {
+    store.addContent(content.value);
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,400;0,500;0,600;0,700;1,100;1,400;1,500;1,600;1,700&display=swap');
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Roboto Mono', monospace;
+  font-size: 10px;
 }
 </style>
---
+++ src/stores/contract.js
@@ -0,0 +1,36 @@
+import { defineStore } from 'pinia';
+import { WarpFactory } from 'warp-contracts/web';
+
+export const useContractStore = defineStore('contract', {
+  state: () => {
+    return {
+      contract: [],
+      contractId: '104Baqn4dDwL7ojuZtbMy5qitBa-WY1_sOai79TJ_Ig',
+      messages: [],
+    };
+  },
+  actions: {
+    async getContract() {
+      const warp = WarpFactory.forMainnet();
+      const contract = warp.contract(this.contractId);
+      // const address = await warp.arweave.wallets.jwkToAddress(wallet);
+      const { cachedValue } = await contract.readState();
+      this.contract = cachedValue.state;
+
+      console.log(this.contract);
+    },
+
+    async addContent(payload) {
+      const warp = WarpFactory.forMainnet();
+      const contract = warp.contract(this.contractId);
+      const wallet = await warp.arweave.wallets.generate();
+      await contract.connect(wallet);
+      await contract.writeInteraction({ function: 'postMessage', content: payload });
+    },
+  },
+  getters: {
+    // contractId: (state) => {
+    //   return state.contractId;
+    // },
+  },
+});
